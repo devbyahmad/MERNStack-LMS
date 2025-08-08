c30a721fabdd047d31cd8e78fe815ed04da25538
Muhammad Ahmad <itxscarface@gmail.com>
2025-08-08 20:49:14 +0500

    Initial commit with frontend and backend

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import app from "./app.js";
+import { connectDb } from './database/db.js'
+import { v2 as cloudinary } from 'cloudinary';
+import Razorpay from 'razorpay'
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+});
+
+ 
+app.listen(process.env.PORT, () => {
+    connectDb()
+    console.log(`server is running on http://localhost:${process.env.PORT}`);
+})
